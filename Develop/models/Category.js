b4d8c97e24f9968606b7ea256c6eb40d0730a3ae
Sean <Seanecrocker@gmail.com>
2024-05-31 20:39:50 -0500

    creating models and importing them into index.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,20 +1,26 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+//Defining Columns//
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 
 module.exports = Category;
