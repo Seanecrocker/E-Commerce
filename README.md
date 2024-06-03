01e719f5fd081f6f1153c4fcc84b090ee8ee926e
Sean <Seanecrocker@gmail.com>
2024-06-03 15:02:33 -0500

    adding code to schema.sql and creating read me

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# E-Commerce
+# E-Commerce Backend
+
+## Description
+
+This project is the backend for an e-commerce website, built using Node.js, Express.js, and Sequelize with a PostgreSQL database. It provides an API for managing products, categories, tags, and product tags.
+
+## Usage
+
+Start the server using: node server.js.
+
+Test routes using insomnia.
+
+## Youtube Link
+https://youtu.be/fOoE2umNWj0
+
+## Github Link
+https://github.com/Seanecrocker/E-Commerce
--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,26 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 10,
+    category_id INTEGER REFERENCES category(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tag (
+    id SERIAL PRIMARY KEY,
+    tag_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product_tag (∞
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES product(id) ON DELETE CASCADE,
+    tag_id INTEGER REFERENCES tag(id) ON DELETE CASCADE
+);
